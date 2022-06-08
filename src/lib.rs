d17367744ed1200b8b86cb573a5d53cf708a6e07
onchezz <56371447+onchezz@users.noreply.github.com>
2022-06-09 00:08:34 +0300

    initial

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,66 @@
+use near_rng::Rng;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
+use near_sdk::{env, log, near_bindgen};
+use std::cmp::Ordering;
 
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
-    // SETUP CONTRACT STATE
+    pub guess: u64,
+}
+impl Default for Contract {
+    fn default() -> Self {
+        Self { guess: 0 }
+    }
 }
 
 #[near_bindgen]
 impl Contract {
+    pub fn random_number(&mut self) -> u64 {
+        let mut rng = Rng::new(&env::random_seed());
+        let value = rng.rand_range_u64(0, 100);
+        self.guess = value;
+        value
+    }
+    pub fn get_user_guess(&mut self, number: u64) {
+        match self.guess.cmp(&number) {
+            Ordering::Less => log!("Too small"),
+            Ordering::Greater => log!("Too big"),
+            Ordering::Equal => log!("You win"),
+        }
+    }
     // ADD CONTRACT METHODS HERE
 }
-
-/*
- * the rest of this file sets up unit tests
- * to run these, the command will be:
- * cargo test --package rust-template -- --nocapture
- * Note: 'rust-template' comes from Cargo.toml's 'name' key
- */
 
 // use the attribute below for unit tests
 #[cfg(test)]
 mod tests {
     use super::*;
-    use near_sdk::test_utils::{get_logs, VMContextBuilder};
-    use near_sdk::{testing_env, AccountId};
+    use near_sdk::test_utils::VMContextBuilder;
+    use near_sdk::{testing_env, AccountId, VMContext};
 
-    // part of writing unit tests is setting up a mock context
-    // provide a `predecessor` here, it'll modify the default context
-    fn get_context(predecessor: AccountId) -> VMContextBuilder {
-        let mut builder = VMContextBuilder::new();
-        builder.predecessor_account_id(predecessor);
-        builder
+    fn contract_account() -> AccountId {
+        "contract".parse::<AccountId>().unwrap()
     }
 
-    // TESTS HERE
+    fn get_context(predecessor_account_id: AccountId) -> VMContext {
+        let mut builder = VMContextBuilder::new();
+        builder
+            .current_account_id(contract_account())
+            .signer_account_id(predecessor_account_id.clone())
+            .predecessor_account_id(predecessor_account_id);
+        builder.build()
+    }
+
+    #[test]
+    fn test_random_number() {
+        let accountid = AccountId::new_unchecked("onchez.test".to_string());
+        let context = get_context(accountid);
+        testing_env!(context);
+
+        let mut contract = Contract::default();
+        let random_number = contract.random_number();
+        assert!(random_number > 0);
+        assert!(random_number < 100);
+    }
 }
